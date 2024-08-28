efc7c921297f8fa8a93da168e5abc80349351643
Student <Student@LEXLINK-HP016>
2024-08-28 14:59:14 +0200

    Add project files.

---
+++ JavaScriptExercise/Program.cs
@@ -0,0 +1,16 @@
+namespace JavaScriptExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+            app.UseFileServer();
+
+            app.Run();
+        }
+    }
+}
